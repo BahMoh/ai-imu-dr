48e809f9d809cc8b631b2645648fc6b788fba1d9
unknown <mohamadai2573@gmail.com>
2024-06-18 19:10:47 +0330

    shapes

--- src/utils_torch_filter.py
+++ src/utils_torch_filter.py
@@ -65,10 +65,10 @@ class MesNet(torch.nn.Module):
             z_cov = self.cov_lin(y_cov)
             z_cov_net = self.beta_measurement.unsqueeze(0)*z_cov
             measurements_covs = (iekf.cov0_measurement.unsqueeze(0) * (10**z_cov_net))
-            print(f"y_cov {y_cov}")
-            print(f"z_cov {z_cov}")
-            print(f"z_cov_net {z_cov_net}")
-            print(f"measurements_covs {measurements_covs}")
+            print(f"y_cov {y_cov.shape}")
+            print(f"z_cov {z_cov.shape}")
+            print(f"z_cov_net {z_cov_net.shape}")
+            print(f"measurements_covs {measurements_covs.shape}")
             return measurements_covs
 
 
